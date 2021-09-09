2e9bc9ae06a3268b768b09e5fcf4db1ed69c3815
Nielsen Pierce <nielsen_pierce@hotmail.com>
2021-09-09 15:52:30 +0100

    update workflow

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from staticjinja import Site
 
 
 if __name__ == "__main__":
-    site = Site.make_site(outpath="docs")
+    site = Site.make_site(outpath="_site")
     # enable automatic reloading
     site.render(use_reloader=False)
